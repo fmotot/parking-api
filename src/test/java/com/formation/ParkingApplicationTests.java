b53049397d64d0d67f48759601d0b9cf8eea413a
Frédérick <fmotot@gmail.com>
2021-03-16 21:29:59 +0100

    test

--- src/test/java/com/formation/ParkingApplicationTests.java
+++ src/test/java/com/formation/ParkingApplicationTests.java
@@ -1,13 +1,17 @@
 package com.formation;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {com.formation.parking.ParkingApplication.class})
 class ParkingApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
+		// assertTrue(false);
 	}
 
 }
